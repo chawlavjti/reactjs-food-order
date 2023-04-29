00bbbb3f17a176395aa4956499f14f4aff92e18f
hukum chawla <chawla.hukum@gmail.com>
2023-04-29 20:30:55 +0100

    commit after merge conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-<<<<<<< HEAD
-//this is new comment
-=======
 //commit by chawlavjti in file directly on github
->>>>>>> ee0dfc4cc589fe77c56948e066c395654203b8da
+
 
 function App() {
   return (
