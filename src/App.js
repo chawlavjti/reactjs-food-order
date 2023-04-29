ee0dfc4cc589fe77c56948e066c395654203b8da
chawlavjti <131971046+chawlavjti@users.noreply.github.com>
2023-04-29 20:26:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+//commit by chawlavjti in file directly on github
 
 function App() {
   return (
